878681852f50d0b0f9516f0f48a216aa678b9e0d
Cameron Braverman <bravermancj@gmail.com>
2021-01-07 11:11:12 -0500

    Initial Commit

---
+++ Shared/SplotchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplotchApp.swift
+//  Shared
+//
+//  Created by Cameron Braverman on 1/7/21.
+//
+
+import SwiftUI
+
+@main
+struct SplotchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
